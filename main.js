2421a49fa4f9a1a8ddcb550381e39ac5fba6a01f
Antonio <amp.olivas00@gmail.com>
2021-11-03 11:50:28 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let gameReason = document.getElementById('reason')
+let faveConsole = document.getElementById('console')
+let faveGame = document.getElementById('game')
+
+function handleSubmit(evt){
+    evt.preventDefault();
+    alert("Welcome!")
+}
+
+
+form.addEventListener('submit' , handleSubmit);
+
